e0d7d0fefac427be1b2b5d1096604876487e0898
Anthony Hillen <ah205@students.uwf.edu>
2019-01-05 06:28:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors = ['red','orange','green','blue','indigo','violet'])
-  puts "R: #{colors[0]}, O: #{colors[1]}, G: #{colors[2]}, B: #{colors[3]}, I: #{colors[4]}, V: #{colors[5]}"
+def display_rainbow(colors = ['red','orange', 'yellow', 'green','blue','indigo','violet'])
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
 
 display_rainbow()
